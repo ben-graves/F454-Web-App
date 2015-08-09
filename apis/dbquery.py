ae69753bd01231aa1a93a36e62eec77d1586cbe0
Ben Graves <bgraves1997@hotmail.co.uk>
2015-08-09 22:00:05 +0100

    add api to create user

--- apis/dbquery.py
+++ apis/dbquery.py
@@ -7,50 +7,15 @@ app = Flask(__name__)
 @app.route("/createuser/firstname=<firstname>/lastname=<lastname>/dob=<dob>/"\
 "emailaddress=<emailaddress>/username=<username>/password=<password>/")
 def createuser(firstname, lastname, dob, emailaddress, username, password):
-    cnx = mysql.connector.connect(user="root", database="E-Flashcards")
+    cnx = mysql.connector.connect(user="root", database="e-flashcards")
     cursor = cnx.cursor()
     query = ("INSERT INTO users"
              "(FirstName, LastName, DoB, EmailAddress, Username, Password)"
              "VALUES (%s, %s, %s, %s, %s, %s)")
-    user_data = (firstname, lastname, dob, emailaddress, lastname, username, password)
+    user_data = (firstname, lastname, dob, emailaddress, username, password)
     cursor.execute(query, user_data)
     cnx.commit()
     return "Success"
 
-@app.route("/edituser/id=<user_id>/username=<username>/firstname=<firstname>/"\
-           "lastname=<lastname>/password=<password>/")
-def edituser(user_id, username, firstname, lastname, password):
-    cnx = mysql.connector.connect(user="root", database="userschema")
-    cursor = cnx.cursor()
-    query = ("""
-             UPDATE users
-             SET username = %s, firstname = %s, lastname = %s, password = %s
-             WHERE id = %s)""")
-    user_data = (user_id, username, firstname, lastname, password)
-    cursor.execute(query, user_data)
-    cnx.commit()
-    return "User edited"
-
-@app.route("/deleteuser/id=<int:user_id>/")
-def deleteuser(user_id):
-    cnx = mysql.connector.connect(user="root", database="userschema")
-    cursor = cnx.cursor()
-    #query = ("DELETE FROM users WHERE ID = %d")
-    user_data = user_id
-    ex = cursor.execute("DELETE FROM 'users' WHERE ID = ?", int(6))
-    cnx.commit()
-    return str(ex)
-
-@app.route("/selectuser/username=<username>/")
-def selectuser(username):
-    cnx = mysql.connector.connect(user="root", database="userschema")
-    cursor = cnx.cursor()
-    query = ("SELECT * FROM users"
-             "WHERE username = %s)")
-    user_data = (username)
-    cursor.execute(query, username)
-    cnx.commit()
-    return cursor
-
 if __name__ == "__main__":
-    app.run(debug=True, port=1234)
+    app.run(debug=True, port=8080)
